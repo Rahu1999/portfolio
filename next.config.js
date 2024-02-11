3354946dfae7d5c9c61aff8b6eb1d201dd876619
= <=>
2024-02-11 23:01:18 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -10,15 +10,11 @@ const nextConfig = {
   //   ],
   // },
   fallback: false,
-  experimental: {
-    serverActions: true,
-  },
   // Add any other configuration options you need
 
   // Configure Next.js to use output: export
   trailingSlash: true,
   output: 'export',
-  
 };
 
 module.exports = nextConfig;
