9c6ed48b80da0c151df5f399d8170d5dd9ca8638
= <=>
2024-02-11 22:48:29 +0530

    update next.config file

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  target: 'serverless', // Required for static HTML export
+  // Add any other configuration options you need
+
+  // Configure Next.js to use output: export
+  output: {
+    // Set the exportPathMap option to define the routes to export
+    // This example exports only the index route '/'
+    // You can add more routes as needed
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' },
+      };
+    },
+  },
 };
 
 module.exports = nextConfig;
