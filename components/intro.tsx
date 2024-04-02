ad21c9381e57e4caa6dededcd49c6b7aa554da8d
Rahul RRR <rahulrajbhar19999@gmail.com>
2024-04-02 14:05:23 +0530

    Remove next from about us

--- components/intro.tsx
+++ components/intro.tsx
@@ -70,7 +70,7 @@ export default function Intro() {
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">{calculateExperience("01-07-2021")} </span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React </span>.
       </motion.h1>
 
       <motion.div
